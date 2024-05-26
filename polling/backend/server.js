d76bff77162d604a5920917a07e616ed908bb561
Farasat Khan <farasatkahan@gmail.com>
2024-05-26 21:26:26 +0500

    added long polling

---
+++ polling/backend/server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import bodyParser from "body-parser";
+import nanobuffer from "nanobuffer";
+import morgan from "morgan";
+
+// set up a limited array
+const msg = new nanobuffer(50);
+const getMsgs = () => Array.from(msg).reverse();
+
+// feel free to take out, this just seeds the server with at least one message
+msg.push({
+  user: "user A",
+  text: "hi",
+  time: Date.now(),
+});
+
+// get express ready to run
+const app = express();
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(express.static("frontend"));
+
+app.get("/poll", function (req, res) {
+  // use getMsgs to get messages to send back
+  // write code here
+  res.json({
+    msg: getMsgs(),
+  });
+});
+
+app.post("/poll", function (req, res) {
+  const { user, text } = req.body;
+
+  msg.push({
+    user,
+    text,
+    time: Date.now(),
+  });
+
+  res.json({
+    success: "ok",
+  });
+});
+
+// start the server
+const port = process.env.PORT || 3000;
+app.listen(port);
+console.log(`listening on http://localhost:${port}`);
